41de8981172d57465a38af4537442a3c5fe7e52d
Samitthichai <Samitthichai28@gmail.com>
2024-02-22 10:53:29 +0700

    add route url

--- web/server.go
+++ web/server.go
@@ -1,9 +1,21 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 // start the web server
 
 func Start() {
 	fmt.Println("Starting server")
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello World")
+	})
+	http.ListenAndServe(":8080", r)
+
 }
